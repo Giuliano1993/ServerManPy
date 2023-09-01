7397479430cb884fd4b0bb5cd426f9e5d29de3c1
Giuliano Gostinfini <giuliano.gostinfini.93@gmail.com>
2023-09-01 21:32:09 +0200

    [add] create ssh connections

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import yaml
+import paramiko
 from colorama import Fore, Back, Style
 
 
@@ -16,4 +17,15 @@ def printFormattedInfo(obj, property, text = '', append = ''):
 def getConfig(configName):
   configsFile = yaml.safe_load(open('./env.yaml'))
   return configsFile['configs'][configName]
+
+
+def createSshConnection(ip):
+  ssh = paramiko.SSHClient()
+  ssh.load_system_host_keys()
+  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  path = getConfig('localKeyFile')
+  if(path == '' or path is None):
+    path = input('Please enter yout SSH Key file path')
+  ssh.connect(ip, username='root',key_filename=path)
+  return ssh
   
