96eca9b25a522c3d03bfcf357b88e3be90bcefdf
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-09-24 18:03:20 +0200

    [ fix ] server creation on netlify

--- platforms/netlify/createServer.py
+++ platforms/netlify/createServer.py
@@ -1,5 +1,7 @@
 import requests
 import datetime
+import time
+import json
 #from ../../ import utilities.utils
 import sys
 sys.path.append("")		# fixes import issues
@@ -9,16 +11,19 @@ import utils
 
 token = utils.getConfig('netlifyToken')
 user = utils.getConfig('netlifyUser')
+sitename = str(time.time())+"site"
+payload = {"name":sitename,"subdomain":sitename}
 response = requests.post(
     url=f'https://api.netlify.com/api/v1/{user}/sites',
     headers={
         "content-type": "application/json",
         "Authorization": "Bearer "+token
     },
-    data= {
-        'name':str(datetime.datetime.now().timestamp())+"site",   
-        'subdomain':str(datetime.datetime.now().timestamp())+"site",   
-    }
+    json=payload
 )
 
-print(response.text)
+if(response.ok):
+    print("New server was correctly created")
+else:
+    print("There was an error:")
+    print(response.status_code + " : " + response.reason)
