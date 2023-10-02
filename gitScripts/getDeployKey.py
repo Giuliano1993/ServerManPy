5be78b83aa713bd0838824b38965c4db75638b77
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-10-02 17:48:47 +0200

    [ add ] local sshkey creation for git addition

--- gitScripts/getDeployKey.py
+++ gitScripts/getDeployKey.py
@@ -1,16 +1,35 @@
 import requests
 import sys
+import os
 sys.path.append("")
 import utils
 
-def createDeployKey(repo, publicKey):
+def createDeployKey(repo):
     user = utils.getConfig("gitUser")
     gitToken = utils.getConfig('gitToken')
+    userMail = utils.getConfig('userMail')
+    if(userMail is not None):
+        mailParam = f"-C {userMail}"
+    else:
+        mailParam = ""
+    
+    home_dir = os.path.expanduser("~")
+    ssh_dir = os.path.join(home_dir, ".ssh")
+    print(ssh_dir)
+    name = input()
+    keyNamePath = ssh_dir + "\git_generated_" + name
+
+    os.system(f'ssh-keygen -q -t ed25519 -N "" -f {keyNamePath} {mailParam}')
+
+    with open(keyNamePath+".pub", "r") as f_in:
+        key = f_in.readline()
+    
+
     url = f"https://api.github.com/repos/{user}/{repo}/keys"
 
     data = {
         "title": "Deploy Key",
-        "key": publicKey,
+        "key": key,
         "read_only": True
     }
     response = requests.post(
